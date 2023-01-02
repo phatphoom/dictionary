6c87859dcfb505d894fce936e71793cfc11f79f7
phatphoom <99587354+phatphoom@users.noreply.github.com>
2023-01-02 23:12:17 +0700

    rename add feature

--- maybe.js
+++ maybe.js
@@ -7,8 +7,13 @@ let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
 let resultw = document.getElementById("result-word")
 let resultph = document.getElementById("result-phonetics")
 let resultm = document.getElementById("result-mean")
-let resultt = document.getElementById("transalate-result")
-let transalate= document.querySelector(".transalate")
+let resultt = document.getElementById("translate-result")
+let translate= document.querySelector(".translate")
+
+function hightlight(string,target){
+    return string.replace(target,`<span style="color:red">${target}</span>`)
+}
+// first call : call the dictonary
 btn.addEventListener("click",()=>{
     fetch(url).then(res=>res.json())
     .then(data=>{
@@ -16,18 +21,20 @@ btn.addEventListener("click",()=>{
             resultw.innerText = data[0].word
             resultph.innerText = data[0].phonetic
             resultm.innerText = data[0].meanings[0].definitions[0].definition
-            transalate.style.display = "block"
+            translate.style.display = "block"
         }
     })
 })
-transalate.addEventListener('click',()=>{
+// second call : call the translate
+translate.addEventListener('click',()=>{
     const lala = resultm.innerText;
     let wt = `https://api.mymemory.translated.net/get?q=${lala}!&langpair=en|th`
     fetch(wt).then(res=>res.json())
     .then(data=>{
+        resultt.innerText =  data.responseData.translatedText
         setTimeout(() => {
-            resultt.innerText =  data.responseData.translatedText
-        }, 4000);
+            resultt.style.display = 'none'
+        }, 2000);
     })
 })
 clearbtn.addEventListener("click",()=>{
